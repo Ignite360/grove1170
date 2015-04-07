59f1a5bddabc4085302a3bd1d7bb8f4e1448da9a
John Coy <johndavidcoy@gmail.com>
2015-04-07 16:49:27 -0500

    Added Slider Order
    Added order control for asc, desc and order type ( date or menu ).

--- wp-content/themes/grove/inc/slider-home.php
+++ wp-content/themes/grove/inc/slider-home.php
@@ -22,7 +22,7 @@
 	$slide_type = get_option('slide_type');
 	
 	if($slide_type == 'posttype') {
-		$mmm = 'wooslider category="'.get_option('slide_pcat').'" slider_type="posts" layout="text-'.get_option('slide_text_side').'" limit="'.get_option("slide_num").'"';
+		$mmm = 'wooslider category="'.get_option('slide_pcat').'" slider_type="posts" layout="text-'.get_option('slide_text_side').'" order="'.get_option("slide_order_by").'" limit="'.get_option("slide_num").'"';
 		
 		if(get_option("slider_linking") != '') { $mmm .= ' link_title="'.get_option("slider_linking").'"'; }
 	
@@ -32,7 +32,7 @@
 		
 	
 	} else if($slide_type == 'sliderstype') {
-		$mmm = 'wooslider slide_page="'.get_option("slide_page").'" slider_type="slides" smoothheight="true" thumbnails="'.get_option("slide_pagination").'" order="DESC" order_by="'.get_option("slide_order_by").'" limit="'.get_option("slide_num").'"';
+		$mmm = 'wooslider slide_page="'.get_option("slide_page").'" slider_type="slides" smoothheight="true" thumbnails="'.get_option("slide_pagination").'" order="'.get_option("slide_order_by").'" order_by="'.get_option("slide_sort_by").'" limit="'.get_option("slide_num").'"';
 		
 		if(get_option("slide_custom_id") != '') { $mmm .= ' id="'.get_option("slide_custom_id").'"'; }
 		
