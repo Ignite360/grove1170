8137ac821827249ac37b4894bf204a35a8648b55
John Coy <johndavidcoy@gmail.com>
2015-04-15 14:37:33 -0500

    Fixed Staff Manager Plugin
    duplicate feature image

--- wp-content/plugins/staff-manager/templates/archive-staff.php
+++ wp-content/plugins/staff-manager/templates/archive-staff.php
@@ -9,56 +9,55 @@
  */
 
 get_header(); ?>
-<?php the_post_thumbnail(  ) ?>
-		<section id="primary" class="full-page-primary">
-			<div id="content-full" class="full-page-content" role="main">
-				<?php echo $image; ?>
+<section id="primary" class="full-page-primary">
+	<div id="content-full" class="full-page-content" role="main">
+		<?php echo $image; ?>
 
-			<?php if ( have_posts() ) : ?>
+	<?php if ( have_posts() ) : ?>
 
-				<header class="entry-header">
-					<h1 class="entry-title">Our Staff</h1>
-				</header><!-- .entry-header -->
+		<header class="entry-header">
+			<h1 class="entry-title">Our Staff</h1>
+		</header><!-- .entry-header -->
 
-				<?php if ( function_exists('grove_content_nav') ) grove_content_nav( 'nav-above' ); ?>
-				<div class="staff-wrapper">
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+		<?php if ( function_exists('grove_content_nav') ) grove_content_nav( 'nav-above' ); ?>
+		<div class="staff-wrapper">
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-					<section class="staff" data-href="<?php the_permalink() ?>">    
+			<section class="staff" data-href="<?php the_permalink() ?>">    
 
-						<div class="container">	
+				<div class="container">	
 
-						    <figure class="left">
-					        	<?php the_post_thumbnail( array( 307, 9999999 ) ) ?>
-					        </figure>
-							
-							<header>
+				    <figure class="left">
+			        	<?php the_post_thumbnail( array( 307, 9999999 ) ) ?>
+			        </figure>
+					
+					<header>
 
-								<i><img src="<?php echo get_option( 'icon_url' ); ?>" alt=""></i>
-								<h1><?php the_title() ?></h1>
-								<div class="meta">
-									<div class="title"><?php echo get_post_meta( get_the_ID(), '_job_title', true ) ?></div>			
-								</div>
-
-							</header>	
-
+						<i><img src="<?php echo get_option( 'icon_url' ); ?>" alt=""></i>
+						<h1><?php the_title() ?></h1>
+						<div class="meta">
+							<div class="title"><?php echo get_post_meta( get_the_ID(), '_job_title', true ) ?></div>			
 						</div>
-					</section>
 
-				<?php endwhile; ?>
+					</header>	
 
-				<?php if ( function_exists('grove_content_nav') ) grove_content_nav( 'nav-below' ); ?>
 				</div>
+			</section>
 
-			<?php else : ?>
+		<?php endwhile; ?>
 
-				<?php get_template_part( 'no-results', 'archive' ); ?>
+		<?php if ( function_exists('grove_content_nav') ) grove_content_nav( 'nav-below' ); ?>
+		</div>
 
-			<?php endif; ?>
+	<?php else : ?>
 
-			</div><!-- #content .site-content -->
-		</section><!-- #primary .content-area -->
+		<?php get_template_part( 'no-results', 'archive' ); ?>
+
+	<?php endif; ?>
+
+	</div><!-- #content .site-content -->
+</section><!-- #primary .content-area -->
 <script>
 jQuery(document).on('click', '[data-href]', function(){
 	window.location.href = jQuery(this).attr('data-href');
